7d095f59f043465953a6fab6328264995f826729
Gireesh Mulage <M1024305@mindtree.com>
2019-05-28 21:33:59 +0530

    files modified

--- src/test/java/com/empmgt/EmpMgtControllerTest.java
+++ src/test/java/com/empmgt/EmpMgtControllerTest.java
@@ -1,15 +1,6 @@
 package com.empmgt;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import com.empmgt.utils.CommonConstant;
-import com.empmgt.vo.EmployeeVo;
-
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.CoreMatchers.equalTo;
+import junit.framework.TestCase;
 
 /**
  * 
@@ -19,54 +10,17 @@ import static org.hamcrest.CoreMatchers.equalTo;
  */
 public class EmpMgtControllerTest {
 
-	@BeforeClass
-	public static void init(){
-		RestAssured.baseURI="http://localhost";
-		RestAssured.port=8080;
-	}
-	
-	@Test
-	@SuppressWarnings("deprecation")
-	public void testAddEmp(){
-		EmployeeVo employeeVo = new EmployeeVo();
-		employeeVo.setUserName("Ram");
-		employeeVo.setPassword("Ram");
-		employeeVo.setFullName("Ram Laxman");
-		employeeVo.setDateOfBirth("05-11-2009");
-		employeeVo.setEmailId("ram@gmail.com");
-		employeeVo.setGender("M");
-		employeeVo.setSecurityQuestion("Whats given name");
-		employeeVo.setSecurityAnswer("Ram");
-		given().contentType(javax.ws.rs.core.MediaType.APPLICATION_JSON).body(employeeVo).when()
-		.put("EmpMgt/addEmp").then().contentType(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-		.body(CommonConstant.STR_STATUS, equalTo(CommonConstant.CONSTANT_200));
-	}
-	
-	@Test
-	public void testDeleteEmployee(){
-		given().contentType(javax.ws.rs.core.MediaType.APPLICATION_JSON).when()
-		.put("EmpMgt/deleteEmp/12").then().contentType(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-		.body(CommonConstant.STR_STATUS, equalTo(CommonConstant.CONSTANT_200));
-	}
-	
-	@Test
-	public void testGetAllEmployeeDetails(){
-		get("EmpMgt/getAllEmpDetails").then().body("statusCode", equalTo(CommonConstant.CONSTANT_200));
-	}
-	
-	@Test
-	public void testGetEmployeeByEmpId(){
-		get("EmpMgt/getByEmpId/3").then().body("employeeVo.userName", equalTo("Gireesh"));
-	}
-	
-	@Test
-	public void testCheckLogin(){
-		EmployeeVo employeeVo = new EmployeeVo();
-		employeeVo.setUserName("Gireesh");
-		employeeVo.setPassword("Gireesh");
-		given().contentType(javax.ws.rs.core.MediaType.APPLICATION_JSON).body(employeeVo).when()
-		.post("EmpMgt/checkLogin").then().
-		contentType(javax.ws.rs.core.MediaType.APPLICATION_JSON)
-		.body(CommonConstant.STR_STATUS, equalTo(CommonConstant.CONSTANT_200));
-	}
+	int value1;
+	int value2;
+	// assigning the values
+	   protected void setUp(){
+	      value1 = 7;
+	      value2 = 8;
+	   }
+
+	   // test method to add two values
+	   public void testAdd(){
+	      double result = value1 + value2;
+	      assertTrue(result == 15);
+	   }
 }
